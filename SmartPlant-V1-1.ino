a52bab930ffdd6d6c1c236f80519e61839fadc38
DIY-Machines <42316159+DIY-Machines@users.noreply.github.com>
2019-04-25 13:15:05 +0100

    Update SmartPlant-V1-1.ino

--- SmartPlant-V1-1.ino
+++ SmartPlant-V1-1.ino
@@ -25,23 +25,23 @@
  */
 
 // These constants won't change. They're used to give names to the pins used:
-const int ledPin = 2; // Digital output pin that the LED is attached to
-const int pumpPin = 12; // Digital output pin that the water pump is attached to
-const int waterLevelPin = A3; // Analoge pin water level sensor is connected to
-const int moistureSensorPin = 7; // Digital input pin used to check the moisture level of the soil
+const int ledPin = 2;                                             // Digital output pin that the LED is attached to
+const int pumpPin = 12;                                           // Digital output pin that the water pump is attached to
+const int waterLevelPin = A3;                                      // Analoge pin water level sensor is connected to
+const int moistureSensorPin = 7;                                  // Digital input pin used to check the moisture level of the soil
 
 
 // These are the values to edit - see the instructional video to find out what needs adjusting and why:
 
-int checkInterval = 1800; //time to wait before checking the soil moisture level - default it to an hour = 1800000
-int waterLevelThreshold = 380; // threshold at which we flash the LED to warn you of a low water level in the pump tank - set this as per the video explains
-int emptyReservoirTimer = 90; // how long the LED will flash to tell us the water tank needs topping up - default it to 900 = 30mins
-int amountToPump = 300; // how long the pump should pump water for when the plant needs it
+double checkInterval = 1800;                                      //time to wait before checking the soil moisture level - default it to an hour = 1800000
+int waterLevelThreshold = 380;                                    // threshold at which we flash the LED to warn you of a low water level in the pump tank - set this as per the video explains
+int emptyReservoirTimer = 90;                                     // how long the LED will flash to tell us the water tank needs topping up - default it to 900 = 30mins
+int amountToPump = 300;                                           // how long the pump should pump water for when the plant needs it
 
+// Global temp values
 
-
-int sensorWaterLevelValue = 0;        // somewhere to store the value read from the waterlevel sensor
-int moistureSensorValue = 0;   //somewhere to store the value read from the soil moisture sensor
+int sensorWaterLevelValue = 0;                                      // somewhere to store the value read from the waterlevel sensor
+int moistureSensorValue = 0;                                       //somewhere to store the value read from the soil moisture sensor
   
 void setup() {
   // put your setup code here, to run once:
@@ -50,26 +50,26 @@ void setup() {
     pinMode(pumpPin, OUTPUT);
     pinMode(moistureSensorPin, INPUT);
 
-  //flash the LED five times to confirm power on and operation of code:
+                                                                  //flash the LED five times to confirm power on and operation of code:
      for (int i=0; i <= 4; i++){
-      digitalWrite(ledPin, HIGH);
-      delay(300);
-      digitalWrite(ledPin, LOW);
-      delay(300);
+        digitalWrite(ledPin, HIGH);
+        delay(300);
+        digitalWrite(ledPin, LOW);
+        delay(300);
       }
       delay(2000);
 
-    digitalWrite(ledPin, HIGH);   // turn the LED on 
+    digitalWrite(ledPin, HIGH);                                   // turn the LED on 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  sensorWaterLevelValue = analogRead(waterLevelPin); //read the value of the water level sensor
-  Serial.print("Water level sensor value: "); //print it to the serial monitor
+  sensorWaterLevelValue = analogRead(waterLevelPin);              //read the value of the water level sensor
+  Serial.print("Water level sensor value: ");                     //print it to the serial monitor
   Serial.println(sensorWaterLevelValue);
 
-  if (sensorWaterLevelValue < waterLevelThreshold){ //check if we need to alert you to a low water level in the tank
+  if (sensorWaterLevelValue < waterLevelThreshold){               //check if we need to alert you to a low water level in the tank
       for (int i=0; i <= emptyReservoirTimer; i++){  
         digitalWrite(ledPin, LOW);
         delay(1000);
@@ -79,25 +79,25 @@ void loop() {
   }
   else {
     digitalWrite(ledPin, HIGH);
-    delay(checkInterval); //wait before checking the soil moisture level
+    delay(checkInterval);                                         //wait before checking the soil moisture level
   }
 
 
 // check soil moisture level
 
-      moistureSensorValue = digitalRead(moistureSensorPin); //read the moisture sensor and save the value
+      moistureSensorValue = digitalRead(moistureSensorPin);       //read the moisture sensor and save the value
       Serial.print("Soil moisture sensor is currently: ");
       Serial.print(moistureSensorValue);
       Serial.println(" ('1' means soil is too dry and '0' means the soil is moist enough.)");
 
       if (moistureSensorValue == 1){
-          //pulse the pump 
+                                                                 //pulse the pump 
         digitalWrite(pumpPin, HIGH);
           Serial.println("pump on");
-        delay(amountToPump);  //keep pumping water
+        delay(amountToPump);                                      //keep pumping water
         digitalWrite(pumpPin, LOW);
          Serial.println("pump off");
-        delay(800); //delay to allow the moisture in the soil to spread through to the sensor
+        delay(800);                                              //delay to allow the moisture in the soil to spread through to the sensor
       }
 
 
